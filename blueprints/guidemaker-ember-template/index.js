7d3d1cd24b23a6cf5c48bacb1036bf3e77bcc65a
Chris Manson <chris.manson@simplabs.com>
2018-11-01 18:30:42 +0000

    removing bootstrap

--- blueprints/guidemaker-ember-template/index.js
+++ blueprints/guidemaker-ember-template/index.js
@@ -13,70 +13,66 @@ module.exports = {
   },
 
   afterInstall() {
-    return this.addAddonsToProject({
-      packages: []
-    }).then(() => {
-      const code = readFileSync('./ember-cli-build.js');
-      const ast = recast.parse(code);
+    const code = readFileSync('./ember-cli-build.js');
+    const ast = recast.parse(code);
 
-      recast.visit(ast, {
-        visitNewExpression: function (path) {
-          var node = path.node;
+    recast.visit(ast, {
+      visitNewExpression: function (path) {
+        var node = path.node;
 
-          if (node.callee.name === 'EmberApp'
-              || node.callee.name === 'EmberAddon') {
-            // console.log(node, node.arguments)
-            const configNode = node.arguments.find(element => element.type === 'ObjectExpression');
+        if (node.callee.name === 'EmberApp'
+            || node.callee.name === 'EmberAddon') {
+          // console.log(node, node.arguments)
+          const configNode = node.arguments.find(element => element.type === 'ObjectExpression');
 
-            let emberPrism = configNode.properties.find(property => {
-              return property.key.value === 'ember-prism'
-            });
+          let emberPrism = configNode.properties.find(property => {
+            return property.key.value === 'ember-prism'
+          });
 
-            if(!emberPrism) {
-              emberPrism = builders.property(
-                'init',
-                builders.literal('ember-prism'),
-                builders.objectExpression([
-                  builders.property(
-                    'init',
-                    builders.identifier('theme'),
-                    builders.literal('okaidia')
-                  ),
-                  builders.property(
-                    'init',
-                    builders.identifier('components'),
-                    builders.arrayExpression([
-                      builders.literal('apacheconf'),
-                      builders.literal('bash'),
-                      builders.literal('css'),
-                      builders.literal('handlebars'),
-                      builders.literal('http'),
-                      builders.literal('javascript'),
-                      builders.literal('json'),
-                      builders.literal('markup-templating'),
-                      builders.literal('ruby'),
-                      builders.literal('scss'),
-                    ])
-                  ),
-                  builders.property(
-                    'init',
-                    builders.identifier('plugins'),
-                    builders.arrayExpression([
-                      builders.literal('line-numbers'),
-                      builders.literal('normalize-whitespace'),
-                    ])
-                  ),
-                ])
-              )
-              configNode.properties.push(emberPrism);
-            }
+          if(!emberPrism) {
+            emberPrism = builders.property(
+              'init',
+              builders.literal('ember-prism'),
+              builders.objectExpression([
+                builders.property(
+                  'init',
+                  builders.identifier('theme'),
+                  builders.literal('okaidia')
+                ),
+                builders.property(
+                  'init',
+                  builders.identifier('components'),
+                  builders.arrayExpression([
+                    builders.literal('apacheconf'),
+                    builders.literal('bash'),
+                    builders.literal('css'),
+                    builders.literal('handlebars'),
+                    builders.literal('http'),
+                    builders.literal('javascript'),
+                    builders.literal('json'),
+                    builders.literal('markup-templating'),
+                    builders.literal('ruby'),
+                    builders.literal('scss'),
+                  ])
+                ),
+                builders.property(
+                  'init',
+                  builders.identifier('plugins'),
+                  builders.arrayExpression([
+                    builders.literal('line-numbers'),
+                    builders.literal('normalize-whitespace'),
+                  ])
+                ),
+              ])
+            )
+            configNode.properties.push(emberPrism);
           }
-
-          this.traverse(path);
         }
-      });
 
-      writeFileSync('./ember-cli-build.js', recast.print(ast, { tabWidth: 2, quote: 'single' }).code);
+        this.traverse(path);
+      }
     });
+
+    writeFileSync('./ember-cli-build.js', recast.print(ast, { tabWidth: 2, quote: 'single' }).code);
   }
 };

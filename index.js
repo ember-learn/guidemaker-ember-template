166391a9c79442b667f344453490f881328316ef
Chris Manson <chris.manson@simplabs.com>
2018-11-02 08:38:56 +0000

    trying to fix ember-cli-sass

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 'use strict';
 
 var Funnel = require('broccoli-funnel');
-const nodeSass = require('node-sass');
 const { join } = require('path');
 
 module.exports = {
   name: 'guidemaker-ember-template',
 
-  included(app, parentAddon) {
-    let target = (app || parentAddon);
-    target.options = target.options || {};
-
-    target.options.sassOptions = target.options.sassOptions || { implementation: nodeSass };
-
-    this._super.included.apply(this, arguments);
-  },
   treeForPublic: function() {
     return new Funnel(join(this.root, 'public'));
   }

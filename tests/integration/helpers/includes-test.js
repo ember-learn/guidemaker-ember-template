30117f731cde475e62f7e05529b6dc6c30c415ce
Ricardo Mendes <rokusu@gmail.com>
2019-03-18 11:56:38 -0700

    add test for includes helper

--- tests/integration/helpers/includes-test.js
+++ tests/integration/helpers/includes-test.js
@@ -1,17 +1,32 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { render } from "@ember/test-helpers";
+import hbs from "htmlbars-inline-precompile";
 
-module('Integration | Helper | includes', function(hooks) {
+module("Integration | Helper | includes", function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test("it returns true if the collection contains the item", async function(assert) {
+    this.set("collection", [1, 2, 3, 4]);
+    this.set("item", 2);
 
-    await render(hbs`{{includes inputValue}}`);
+    await render(hbs`{{if (includes collection item) 'yes' 'no'}}`);
+    assert.dom(this.element).hasText("yes");
+  });
 
-    assert.equal(this.element.textContent.trim(), '1234');
+  test("it returns false if the collection is null", async function(assert) {
+    this.set("collection", null);
+    this.set("item", 2);
+
+    await render(hbs`{{if (includes collection item) 'yes' 'no'}}`);
+    assert.dom(this.element).hasText("no");
+  });
+
+  test("it returns false if the collection is undefined", async function(assert) {
+    this.set("collection", undefined);
+    this.set("item", 2);
+
+    await render(hbs`{{if (includes collection item) 'yes' 'no'}}`);
+    assert.dom(this.element).hasText("no");
   });
 });

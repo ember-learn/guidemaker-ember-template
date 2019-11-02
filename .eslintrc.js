b58ceef825912fea1d87da2334caae64b5c75bf8
Jen Weber <j@jenweber.me>
2019-11-02 14:44:03 -0400

    update Ember version to 3.12

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [
@@ -40,8 +40,7 @@ module.exports = {
         'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Compatibility
 
 * Ember.js v3.4 or above
 * Ember CLI v2.13 or above
+* Node.js v8 or above
 
 
 Installation

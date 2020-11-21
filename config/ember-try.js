fc1b226c998e4c90b8615c12e605c472e012e28a
ijlee2 <ijlee2@users.noreply.github.com>
2020-11-21 13:03:08 -0600

    Allowed canary test scenario to fail, as before

--- config/ember-try.js
+++ config/ember-try.js
@@ -51,6 +51,7 @@ module.exports = function() {
           },
         },
         {
+          allowedToFail: true,
           name: 'ember-canary',
           npm: {
             devDependencies: {

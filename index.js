90ab4f0487f675a347725e0f2af628e56aabc5c0
Chris Manson <chris@manson.ie>
2021-02-19 09:18:54 +0000

    use ember-showdown-prism for code blocks

--- index.js
+++ index.js
@@ -19,29 +19,4 @@ module.exports = {
   treeForPublic: function() {
     return new Funnel(join(this.root, 'public'));
   },
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    if (!app.options['ember-prism']) {
-      app.options['ember-prism'] = {
-        theme: 'okaidia',
-
-        components: [
-          'apacheconf',
-          'bash',
-          'css',
-          'handlebars',
-          'http',
-          'javascript',
-          'json',
-          'markup-templating',
-          'ruby',
-          'scss',
-        ],
-
-        plugins: ['line-numbers', 'normalize-whitespace'],
-      };
-    }
-  },
 };

128de249c02c3032d3c873543f1e40b8c4f5865e
Chris Manson <chris@manson.ie>
2021-02-25 21:39:29 +0000

    use composible-helpers includes helper

--- addon/helpers/includes.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { helper } from "@ember/component/helper";
-
-export function includes([collection, item] /*, hash*/) {
-  return (collection || []).includes(item);
-}
-
-export default helper(includes);

5d078a63887a58bf14c169410d521b76d8a47760
Chris Manson <chris@manson.ie>
2021-04-01 17:50:39 +0100

    fixing TableOfContents component and converting to temolate only

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -1,15 +1,1 @@
-/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier */
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
-
-export default Component.extend({
-  fastboot: service(),
-
-  level: '0',
-  tagName: 'ol',
-  tocLevel: computed('level', function() {
-    return `toc-level-${this.level}`;
-  }),
-  classNameBindings: ['tocLevel'],
-});
+export { default } from 'guidemaker-ember-template/components/table-of-contents';

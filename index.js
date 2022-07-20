ef18c92d0d085f04d06c1b7f35ee7f5e7d654940
Chris Manson <chris@manson.ie>
2022-07-20 15:49:37 +0100

    update algoliasearch dependency

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const { join } = require('path');
 module.exports = {
   name: require('./package').name,
 
-  options: {
-    autoImport: {
-      webpack: {
-        node: {
-          process: true,
-        },
-      },
-    },
-  },
-
   treeForPublic: function() {
     return new Funnel(join(this.root, 'public'));
   },

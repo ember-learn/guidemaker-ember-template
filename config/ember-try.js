54b66e2968c60dde2c6876722032e98e4ebf000e
Chris Manson <chris@manson.ie>
2023-04-17 13:27:36 +0100

    drop support for ember < 3.28

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,24 +7,6 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.20',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.20.5',
-            'ember-named-blocks-polyfill': '*',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3',
-            'ember-named-blocks-polyfill': '*',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {

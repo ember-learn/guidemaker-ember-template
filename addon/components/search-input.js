b8b7687cb763edc6cfa6855ebd12e8de16f28c1b
ignace <ignace@otainsight.com>
2023-08-05 17:51:19 +0200

    fix: clean up old algolia code

--- addon/components/search-input.js
+++ addon/components/search-input.js
@@ -1,10 +1,6 @@
 /* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier, ember/no-get, ember/no-actions-hash */
 import { getOwner } from '@ember/application';
 import Component from '@ember/component';
-import { get, set } from '@ember/object';
-import { and } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
-import { task, timeout } from 'ember-concurrency';
 
 import layout from '../templates/components/search-input';
 
@@ -12,30 +8,10 @@ import docsearch from '@docsearch/js';
 
 import '@docsearch/css';
 
-const SEARCH_DEBOUNCE_PERIOD = 300;
-const SEARCH_CLOSE_PERIOD = 200;
-
 export default Component.extend({
   layout,
 
   classNames: ['search-input'],
-
-  searchService: service('search'),
-
-  _resultTetherConstraints: Object.freeze([
-    {
-      to: 'window',
-      pin: ['left', 'right'],
-    },
-  ]),
-
-  _focused: false,
-
-  init() {
-    this._super(...arguments);
-    const config = getOwner(this).resolveRegistration('config:environment');
-    this.deprecationsGuideURL = config['deprecationsGuideURL'];
-  },
 
   didInsertElement() {
     const config = getOwner(this).resolveRegistration('config:environment');
@@ -60,39 +36,5 @@ export default Component.extend({
     }
 
     docsearch(docsearchParams);
-  },
-
-  showDropdown: and('query', '_focused'),
-
-  search: task(function* (query) {
-    yield timeout(SEARCH_DEBOUNCE_PERIOD);
-
-    set(this, 'query', query);
-
-    // Hide and don't run query if there's no search query
-    if (!query) {
-      return set(this, '_focused', false);
-    }
-
-    // ensure search results are visible if the menu was previously closed above
-    set(this, '_focused', true);
-
-    yield get(this, 'searchService.search').perform(query, this.projectVersion);
-  }).restartable(),
-
-  closeMenu: task(function* () {
-    yield timeout(SEARCH_CLOSE_PERIOD);
-
-    set(this, '_focused', false);
-  }),
-
-  actions: {
-    onfocus() {
-      set(this, '_focused', true);
-    },
-
-    onblur() {
-      this.get('closeMenu').perform();
-    },
   },
 });

cc8bf16b3754eba23bc02d881b4bc1e682fa9c55
Chris Manson <chris@manson.ie>
2024-03-01 13:34:27 +0000

    only use mirage in tests

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -1,20 +1,22 @@
-/* eslint-disable prettier/prettier */
 import searchResponse from './fixtures/search-response';
 
 import { createServer } from 'miragejs';
 export default function makeServer(config) {
   let finalConfig = {
-    ...config,
+    factories: config.factories,
+    models: config.models,
     routes,
   };
+
   return createServer(finalConfig);
 }
+
 function routes() {
   this.post(
     'https://FAKEKEY-dsn.algolia.net/1/indexes/ember-guides/query',
     () => {
       return searchResponse;
-    }
+    },
   );
 
   this.passthrough();

e4b34feec5a839408b1993fb18cc09c1a146bfd7
Chris Manson <chris@manson.ie>
2024-04-10 22:19:36 +0100

    bring fingerprinting config back

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -7,6 +7,9 @@ module.exports = function (defaults) {
     autoImport: {
       watchDependencies: ['guidemaker-ember-template'],
     },
+    fingerprint: {
+      extensions: ['js', 'css', 'map'],
+    },
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');

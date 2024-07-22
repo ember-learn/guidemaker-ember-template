b5edbce221798e155998c813cf0e9281352b54df
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2024-07-22 20:10:48 +0200

    refactor: run prettier on guides-article and version templates

--- guidemaker-ember-template/src/components/guides-article.js
+++ guidemaker-ember-template/src/components/guides-article.js
@@ -4,8 +4,8 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 function wrap(el, wrapper) {
-    el.parentNode.insertBefore(wrapper, el);
-    wrapper.appendChild(el);
+  el.parentNode.insertBefore(wrapper, el);
+  wrapper.appendChild(el);
 }
 
 export default Component.extend({
@@ -13,13 +13,13 @@ export default Component.extend({
   classNames: 'chapter',
   page: service(),
   guidemaker: service(),
-  editVersion: computed('version', 'currentVersion', function() {
-    if(this.page.currentVersion === 'release') {
+  editVersion: computed('version', 'currentVersion', function () {
+    if (this.page.currentVersion === 'release') {
       return '';
     }
 
-    if(this.version === this.currentVersion) {
-      return 'release/'
+    if (this.version === this.currentVersion) {
+      return 'release/';
     }
 
     return `${this.page.currentVersion}/`;
--- guidemaker-ember-template/src/templates/version/index.gjs
+++ guidemaker-ember-template/src/templates/version/index.gjs
@@ -3,6 +3,11 @@ import RouteTemplate from 'ember-route-template';
 import GuidesArticle from '../../components/guides-article';
 
 export default RouteTemplate(<template>
-{{!-- template-lint-disable no-curly-component-invocation no-implicit-this --}}
-<GuidesArticle @model={{@controller.model.content}} @pages={{@controller.model.pages}} @path="index" @version={{@controller.model.version}} @currentVersion={{@controller.model.currentVersion}} />
+  <GuidesArticle
+    @model={{@controller.model.content}}
+    @pages={{@controller.model.pages}}
+    @path='index'
+    @version={{@controller.model.version}}
+    @currentVersion={{@controller.model.currentVersion}}
+  />
 </template>);

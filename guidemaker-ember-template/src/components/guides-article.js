2a3b220146e758f4c0006e964889b0d90ad0f95f
Chris Manson <chris@manson.ie>
2025-09-26 17:36:41 +0100

    make the banner show on a version that is configurable

--- guidemaker-ember-template/src/components/guides-article.js
+++ guidemaker-ember-template/src/components/guides-article.js
@@ -1,9 +1,23 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
+// TODO remove this and use appEmberSatisfies() from @embroider/macros when it's done
+import config from 'ember-get-config';
+
 export default class GuidesArticleComponent extends Component {
   @service page;
   @service guidemaker;
+
+  get gjsLink() {
+    return config?.guidemaker?.gjsLink;
+  }
+
+  get showGJSBanner() {
+    return (
+      Array.isArray(config?.guidemaker?.gjsVersions) &&
+      Boolean(config.guidemaker.gjsVersions.includes(this.args.version))
+    );
+  }
 
   get editVersion() {
     if (this.page.currentVersion === 'release') {
